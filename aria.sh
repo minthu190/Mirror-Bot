19349e7f63c02935dd978326d784c3cac4092a6c
KenHV <yo@kenharris.xyz>
2021-12-19 14:03:18 +0530

    aria: Edit tracker list
    Change-Id: I49477bbd949f36f8dda9b139a7e0bc2f235b7107
    Signed-off-by: KenHV <yo@kenharris.xyz>

--- aria.sh
+++ aria.sh
@@ -2,7 +2,7 @@ if [ -z $TORRENT_TIMEOUT ]
 then
    TORRENT_TIMEOUT=0
 fi
-tracker_list=$(curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt https://ngosang.github.io/trackerslist/trackers_all_http.txt https://newtrackon.com/api/all https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_tracker.txt | awk '$0' | tr '\n\n' ',')
+tracker_list=$(curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all_udp.txt | awk '$0' | tr '\n' ',')
 aria2c --enable-rpc=true --check-certificate=false --daemon=true \
    --max-connection-per-server=10 --rpc-max-request-size=1024M --quiet=true \
    --bt-stop-timeout=$TORRENT_TIMEOUT --min-split-size=10M --split=10 --allow-overwrite=true \
@@ -12,4 +12,4 @@ aria2c --enable-rpc=true --check-certificate=false --daemon=true \
    --bt-enable-lpd=true --seed-time=0 --max-file-not-found=0 --max-tries=20 \
    --auto-file-renaming=true --reuse-uri=true --http-accept-gzip=true --continue=true \
    --content-disposition-default-utf8=true --netrc-path=/usr/src/app/.netrc
-   
+
